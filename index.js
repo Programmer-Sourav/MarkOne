4e99596da52e6cbb43c62cb38bfac4ea5c2e8a0c
Programmer-Sourav <appsbucket333@gmail.com>
2022-12-11 20:11:33 +0530

    fix updated code

--- index.js
+++ index.js
@@ -1,59 +1,91 @@
-/*var readlineSync = require('readline-sync')
-var username = readlineSync.question("May I have your name please!")
-var welcomeMessage = "Welcome!" + username;
-console.log(welcomeMessage)*/
-
-
-/***** //function to add two numbers
-  
-function addTwoNumbers(number1, number2){
-  var sum = number1+number2
-  return sum
-}
-
-var result = addTwoNumbers(2,5)
-console.log("Result Sum "+result)****/
+//output your name - ex1
+//console.log("Sourav Nath")
+//input user's name - ex2
+/*
 
 /**** Mark 1 *****/
 
 var readLineSync = require('readline-sync')
 
+var username = readLineSync.question("May I have your name please?")
+var welcomeUser = "Welcome, " + username
+console.log(welcomeUser)
+
 var question1 = "What is my favourite food?"
 var question2 = "What is my favourite color?"
 var question3 = "When we will go to Goa?"
+var question4 = "What do I do?"
+var question5 = "Which is my favourite city in India?"
+var question6 = "Am I a lazy fellow?"
+
+var answer1 = "chicken"
+var answer2 = "red"
+var answer3 = "Never"
+var answer4 = "Freelancing"
+var answer5 = "Bengaluru"
+var answer6 = "No"
+
 var score = 0
 
-function play(question, answer){
+
+function play(question, answer) {
   var userAnswer1 = readLineSync.question(question1)
 
-  if(userAnswer1 == "Chicken"){
+  if (userAnswer1.toLowerCase() === answer1.toLowerCase()) {
     console.log("Great! You're right")
-    score = score+1
+    score = score + 1
   }
-  else{
+  else {
     console.log("Oops!You're Wrong!")
-  
+
   }
-var userAnswer2 = readLineSync.question(question2)
-   if(userAnswer2 == "Red"){
+  var userAnswer2 = readLineSync.question(question2)
+  if (userAnswer2.toLowerCase() === answer2.toLowerCase()) {
     console.log("Great! You're right")
-    score = score+1
+    score = score + 1
   }
-  else{
+  else {
     console.log("Oops!You're Wrong!")
-    
+
   }
   var userAnswer3 = readLineSync.question(question3)
-   if(userAnswer3 == "Never"){
+  if (userAnswer3.toLowerCase() === answer3.toLowerCase()) {
     console.log("Great! You're right")
-    score = score+1
+    score = score + 1
   }
-  else{
+  else {
     console.log("Oops!You're Wrong!")
-   
+
   }
-console.log("Your Score is "+score)
+  var userAnswer4 = readLineSync.question(question4)
+  if (userAnswer4.toLowerCase() == answer4.toLowerCase()) {
+    console.log("Great! You're right")
+    score = score + 1
+  }
+  else {
+    console.log("Oops!You're Wrong!")
+
+  }
+  var userAnswer5 = readLineSync.question(question5)
+  if (userAnswer5.toLowerCase() == answer5.toLowerCase()) {
+    console.log("Great! You're right")
+    score = score + 1
+  }
+  else {
+    console.log("Oops!You're Wrong!")
+
+  }
+  var userAnswer6 = readLineSync.question(question6)
+  if (userAnswer6.toLowerCase() == answer6.toLowerCase()) {
+    console.log("Great! You're right")
+    score = score + 1
+  }
+  else {
+    console.log("Oops!You're Wrong!")
+
+  }
+  console.log(username + " Your Score is " + score)
 }
 
-play("Where do you work? ","Google")
+play("Where do you work? ", "Dreaming")
 
